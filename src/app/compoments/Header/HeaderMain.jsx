7e1772362dca0862ced9fe966031e103af853898
Vitalik1788 <tarasenkovitalik2013@ukr.net>
2024-12-08 14:08:50 +0200

    mobile header

--- src/app/compoments/Header/HeaderMain.jsx
+++ src/app/compoments/Header/HeaderMain.jsx
@@ -1,11 +1,30 @@
+'use client';
+
 import css from './HeaderMain.module.css';
 import Image from 'next/image';
 import logo from '../../../../public/images/logo-mobile-orange.jpg';
+import { RxHamburgerMenu } from 'react-icons/rx';
+import { FaUser } from 'react-icons/fa6';
+import { useState } from 'react';
 
 export default function HeaderMain() {
+  const [isLoggedin, setIsLoggedin] = useState(true);
+
   return (
-    <div className={css.headerBox}> 
-      <Image className={css.logoIMG} src={logo} alt='logo' priority={true} />
+    <div className={css.headerBox}>
+      <Image className={css.logoIMG} src={logo} alt="logo" priority={true} />
+      <div className={css.menuBox}>
+        {isLoggedin && (
+          <div className={css.userIconBox}>
+            <FaUser size={20} className={css.userIcon} />
+          </div>
+        )}
+
+        <RxHamburgerMenu
+          size={24}
+          style={{ color: 'white', cursor: 'pointer' }}
+        />
+      </div>
     </div>
-  )
+  );
 }
