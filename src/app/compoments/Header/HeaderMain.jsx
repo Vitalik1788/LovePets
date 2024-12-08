bec9bc568fbce3f836cec2df74dccd40a82f65bd
Vitalik1788 <tarasenkovitalik2013@ukr.net>
2024-12-08 15:31:12 +0200

    finish header

--- src/app/compoments/Header/HeaderMain.jsx
+++ src/app/compoments/Header/HeaderMain.jsx
@@ -8,21 +8,32 @@ import { FaUser } from 'react-icons/fa6';
 import { useState } from 'react';
 
 export default function HeaderMain() {
-  const [isLoggedin, setIsLoggedin] = useState(true);
+  const [isLoggedin, setIsLoggedin] = useState(false);
 
   return (
     <div className={css.headerBox}>
       <Image className={css.logoIMG} src={logo} alt="logo" priority={true} />
+      <div className={css.navButtonsBox}>
+        <ul className={css.navButtonsList}>
+          <li className={css.navButtonsItem}>News</li>
+          <li className={css.navButtonsItem}>Find pet</li>
+          <li className={css.navButtonsItem}>Our friends</li>
+        </ul>
+      </div>
       <div className={css.menuBox}>
-        {isLoggedin && (
+        {isLoggedin ? (
           <>
             <div className={css.userIconBox}>
               <FaUser className={css.userIcon} />
             </div>
             <p className={css.userName}>Anna</p>
           </>
+        ) : (
+          <div className={css.authbtngroup}>
+            <button className={css.logInBtn}>Log In</button>
+            <button className={css.regBtn}>Registration</button>
+          </div>
         )}
-
         <RxHamburgerMenu className={css.burgerMenuIcon} />
       </div>
     </div>
